f8ad2c32185f89cf8d76578714527de30aae2698
ashup <ashupal0089@gmail.com>
2024-02-28 00:51:39 +0530

    28-02-24

--- lib/main.dart
+++ lib/main.dart
@@ -26,6 +26,7 @@ class _clockState extends State<clock> {
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         body: Container(
+          padding: EdgeInsets.all(20),
           height: double.maxFinite,
           width: double.maxFinite,
           decoration: BoxDecoration(
@@ -36,48 +37,51 @@ class _clockState extends State<clock> {
           child: Column(
             children: [
               Padding(
-                padding: const EdgeInsets.only(top: 50),
+                padding: const EdgeInsets.only(top: 10),
                 child: Stack(
                   children: [
-                    Container(
-                      width: 250,
-                      height: 250,
-                      decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(280),
-                          boxShadow: [
-                            BoxShadow(
-                                offset: Offset(-60, 110),
-                                color: Color(0xff7E5F64),
-                                blurRadius: 80,
-                                spreadRadius: -30)
-                          ]),
-                      child: ClipRRect(
-                        borderRadius: BorderRadius.circular(90),
-                        child: Image.asset(
-                          'assets/images/moon.png',
-                          height: 350,
-                          fit: BoxFit.cover,
-                        ),
-                      ),
-                    ),
-                    Container(
-                      decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(100),
-                          boxShadow: [
-                            BoxShadow(
-                                offset: Offset(30, 0),
-                                color: Color(0xffFEA66A),
-                                blurRadius: 30,
-                                spreadRadius: -90)
-                          ]),
-                      child: ClipRRect(
-                        borderRadius: BorderRadius.circular(90),
-                        child: Image.asset(
-                          'assets/images/stars.png',
-                          fit: BoxFit.cover,
-                        ),
-                      ),
-                    ),
+                    // Container(
+                    //   width: 250,
+                    //   height: 250,
+                    //   decoration: BoxDecoration(
+                    //       borderRadius: BorderRadius.circular(280),
+                    //       boxShadow: [
+                    //         BoxShadow(
+                    //             offset: Offset(-60, 110),
+                    //             color: Color(0xff7E5F64),
+                    //             blurRadius: 80,
+                    //             spreadRadius: -30)
+                    //       ]),
+                    //   child: ClipRRect(
+                    //     borderRadius: BorderRadius.circular(90),
+                    //     child: Image.asset(
+                    //       'assets/images/moon.png',
+                    //       height: 350,
+                    //       fit: BoxFit.cover,
+                    //     ),
+                    //   ),
+                    // ),
+                    // Container(
+                    //   decoration: BoxDecoration(
+                    //       borderRadius: BorderRadius.circular(100),
+                    //       boxShadow: [
+                    //         BoxShadow(
+                    //             offset: Offset(30, 0),
+                    //             color: Color(0xffFEA66A),
+                    //             blurRadius: 30,
+                    //             spreadRadius: -90)
+                    //       ]),
+                    //   child: ClipRRect(
+                    //     borderRadius: BorderRadius.circular(90),
+                    //     child: Image.asset(
+                    //       'assets/images/stars.png',
+                    //       fit: BoxFit.cover,
+                    //     ),
+                    //   ),
+                    // ),
+                    (time.hour >= 6 && time.hour <= 19)
+                        ? Image.asset('assets/images/good-morning-my-love.gif')
+                        : Image.asset('assets/images/giphy.gif')
                   ],
                 ),
               ),
@@ -85,13 +89,17 @@ class _clockState extends State<clock> {
                 height: 20,
               ),
               Padding(
-                padding: const EdgeInsets.all(20),
+                padding: const EdgeInsets.only(top: 10),
                 child: Row(
                   mainAxisAlignment: MainAxisAlignment.start,
                   children: [
                     Text.rich(TextSpan(children: [
                       TextSpan(
-                        text: "${time.hour % 12}",
+                        text: (time.hour > 12 && time.hour <= 24)
+                            ? "${time.hour % 12}"
+                            : (time.hour == 0)
+                                ? "${12}"
+                                : "0${time.hour}",
                         style: GoogleFonts.inter(
                             color: Colors.white,
                             fontSize: 80,
@@ -105,7 +113,9 @@ class _clockState extends State<clock> {
                             fontWeight: FontWeight.w500),
                       ),
                       TextSpan(
-                        text: "${time.minute}",
+                        text: (time.minute > 9)
+                            ? "${time.minute}"
+                            : "0${time.minute}",
                         style: GoogleFonts.inter(
                             color: Colors.white,
                             fontSize: 80,
@@ -122,59 +132,68 @@ class _clockState extends State<clock> {
                   ],
                 ),
               ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 15),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Text(
+                      "${time.day}-${time.month}-${time.year}",
+                      style: GoogleFonts.inter(
+                          color: Colors.white,
+                          fontSize: 25,
+                          fontWeight: FontWeight.w600),
+                    ),
+                  ],
+                ),
+              ),
               Align(
                 alignment: Alignment.bottomCenter,
-                heightFactor: 4.8,
-                child: Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 10),
-                  child: GlassContainer(
-                    height: 80,
-                    width: double.maxFinite,
-                    blur: 4,
-                    color: Colors.white.withOpacity(0.1),
-                    border: Border.fromBorderSide(BorderSide.none),
-                    borderRadius: BorderRadius.circular(60),
-                    child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                        children: List.generate(
-                            4,
-                            (index) => InkWell(
-                                  onTap: () {
-                                    setState(() {
-                                      click = index;
-                                    });
-                                  },
-                                  child: (click == index)
-                                      ? Container(
-                                          height: 60,
-                                          width: 80,
-                                          decoration: BoxDecoration(
-                                              color: Color(0xffd7a6ff),
-                                              borderRadius:
-                                                  BorderRadius.circular(70)),
-                                          child: IconButton(
-                                              onPressed: () {},
-                                              icon: Icon(
-                                                icons[index],
-                                                size: 35,
-                                              )),
-                                        )
-                                      : Container(
-                                          height: 60,
-                                          width: 80,
-                                          decoration: BoxDecoration(
-                                              color: Colors.transparent,
-                                              borderRadius:
-                                                  BorderRadius.circular(70)),
-                                          child: IconButton(
-                                              onPressed: () {},
-                                              icon: Icon(
-                                                icons[index],
-                                                size: 35,
-                                              )),
+                heightFactor: 3.5,
+                child: GlassContainer(
+                  height: 80,
+                  width: double.maxFinite,
+                  blur: 10,
+                  color: Colors.white.withOpacity(0.1),
+                  border: Border.fromBorderSide(BorderSide.none),
+                  borderRadius: BorderRadius.circular(60),
+                  child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: List.generate(
+                          4,
+                          (index) => InkWell(
+                                onTap: () {
+                                  setState(() {
+                                    click = index;
+                                  });
+                                },
+                                child: (click == index)
+                                    ? Container(
+                                        height: 60,
+                                        width: 100,
+                                        decoration: BoxDecoration(
+                                            color: Color(0xffd7a6ff),
+                                            borderRadius:
+                                                BorderRadius.circular(70)),
+                                        child: Icon(
+                                          icons[index],
+                                          size: 35,
                                         ),
-                                ))),
-                  ),
+                                      )
+                                    : Container(
+                                        height: 60,
+                                        width: 50,
+                                        decoration: BoxDecoration(
+                                            color: Colors.transparent,
+                                            borderRadius:
+                                                BorderRadius.circular(70)),
+                                        child: Icon(
+                                          icons[index],
+                                          size: 35,
+                                          color: Colors.grey.shade500,
+                                        ),
+                                      ),
+                              ))),
                 ),
               ),
             ],
@@ -183,25 +202,7 @@ class _clockState extends State<clock> {
       ),
     );
   }
-
-  Widget iconcont(IconData icon) {
-    return InkWell(
-      onTap: () {},
-      child: Container(
-        height: 60,
-        width: 80,
-        decoration: BoxDecoration(
-            color: Color(0xffd7a6ff), borderRadius: BorderRadius.circular(70)),
-        child: IconButton(
-            onPressed: () {},
-            icon: Icon(
-              icon,
-              size: 35,
-            )),
-      ),
-    );
-  }
 }
 
 List icons = [Icons.alarm, Icons.all_inclusive, Icons.timer, Icons.schedule];
-var click = 0;
+int click = 0;
